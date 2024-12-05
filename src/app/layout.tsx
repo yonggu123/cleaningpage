1fca3681239f89c547981f30a44de1493a9eae6e
yonggu123 <ucan9404@gmail.com>
2024-12-05 20:32:16 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-adsense-account" content="ca-pub-6238144034010969">
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
